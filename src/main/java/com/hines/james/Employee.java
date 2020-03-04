9e5b51db437bb47abd45b8502b8e8cc59f582a76
James Hines <jhines2k7@yahoo.com>
2020-03-04 21:55:56 +0000

    Initial commit

---
+++ src/main/java/com/hines/james/Employee.java
@@ -0,0 +1,12 @@
+public class Employee {
+
+  public Employee(int id, String name, float salary) {
+    this.id = id;
+    this.name = name;
+    this.salary = salary;    
+  }
+
+  int id;
+  String name;
+  float salary;
+}
---
+++ src/test/java/com/hines/james/AppTest.java
@@ -0,0 +1,46 @@
+package com.hines.james;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+
+    private static Employee[] arrayOfEmps
+
+    @Test
+    public void when_increment_salary_for_each_employee_then_apply_new_salary() {
+      
+    }
+
+}
